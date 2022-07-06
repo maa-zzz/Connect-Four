f4c2e50d3e90e454510347778826380f2c714fe9
Md Maaz <84639456+maa-zzz@users.noreply.github.com>
2022-07-06 22:32:21 +0530

    Update ConnectGame.js

--- Connect Four/ConnectGame.js
+++ Connect Four/ConnectGame.js
@@ -32,7 +32,7 @@ function checkBottom(colIndex){
 }
 
 function colorMatchCheck(one,two,three,four){
-    return(one===two && one===three && one==four && one!= 'rgba(128, 128, 128, 0.595)' && one !== undefined)
+    return(one===two && one===three && one===four && one!== 'rgba(128, 128, 128, 0.595)' && one !== undefined)
 }
 
 function horizontalwincheck(){
